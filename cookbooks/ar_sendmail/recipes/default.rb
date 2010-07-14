c48cb070fbad55d14721f1fc722019491f0b79b5
Jatinder Singh <jatinder.saundh@gmail.com>
2010-07-14 13:34:20 -0700

    Added ar_sendmail chef recipe

---
+++ cookbooks/ar_sendmail/recipes/default.rb
@@ -0,0 +1,34 @@
+appname = "theauteurscom2"
+
+if %w(solo app app_master).include?(node[:instance_role])
+  user = node[:owner_name]
+  framework_env = node[:environment][:framework_env]
+
+  run_for_app appname do |app_name, data|
+    worker_name = "ar_mailer_#{app_name}"
+
+    directory "/data/#{app_name}/shared/pids" do
+      owner user
+      group user
+      mode 0755
+    end
+
+    template "/etc/monit.d/ar_sendmail.#{app_name}.monitrc" do
+      source 'ar_sendmail.monitrc.erb'
+      owner 'root'
+      group 'root'
+      mode 0644
+      variables(
+        :app_name => app_name,
+        :user => user,
+        :worker_name => worker_name,
+        :framework_env => framework_env
+      )
+    end
+
+    bash 'monit-reload-restart' do
+      user 'root'
+      code 'monit reload && monit'
+    end
+  end
+end
